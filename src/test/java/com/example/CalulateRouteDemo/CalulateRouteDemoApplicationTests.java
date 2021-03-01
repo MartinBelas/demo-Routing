eb142ab43b7c6c45c2ccfbfe39f5979b03130ed7
Martin Belas <Martin.Belas@outlook.com>
2021-03-01 10:26:10 +0100

    Initial commit

---
+++ src/test/java/com/example/CalulateRouteDemo/CalulateRouteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CalulateRouteDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalulateRouteDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
