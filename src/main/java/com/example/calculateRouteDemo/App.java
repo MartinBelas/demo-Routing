0749c3320586b592fd1e1ec755c389e20603bbea
Martin Belas <Martin.Belas@outlook.com>
2021-03-02 10:41:15 +0100

    Style

--- src/main/java/com/example/calculateRouteDemo/App.java
+++ src/main/java/com/example/calculateRouteDemo/App.java
@@ -10,7 +10,7 @@ public class App extends SpringBootServletInitializer {
     public static void main(String[] args) {
         
         new App()
-                .configure(new SpringApplicationBuilder(App.class))
-                .run(args);
+            .configure(new SpringApplicationBuilder(App.class))
+            .run(args);
     }
 }
